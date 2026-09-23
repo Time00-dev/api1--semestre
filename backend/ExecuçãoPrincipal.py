6414dc8afd46dc6e67cd121a4227e6d139c5bcc1
João Gabriel <joaogabrielbordinhon@gmail.com>
2026-09-23 10:36:16 -0300

    atualizando execução principal e eixo I (#43)

--- backend/ExecuçãoPrincipal.py
+++ backend/ExecuçãoPrincipal.py
@@ -1,16 +1,16 @@
 # --- Extração e Pré-Processamento ---
-processar()[cite: 1]
+processar()
 
-construir_df_scr_pix()[cite: 1]
-construir_df_ibge()[cite: 1]
+construir_df_scr_pix()
+construir_df_ibge()
 
-df_scr_pix = extract_data('/content/dados/scr_pix.parquet')[cite: 1]
-df_ibge = extract_data('/content/dados/ibge.parquet')[cite: 1]
+df_scr_pix = extract_data('/content/dados/scr_pix.parquet')
+df_ibge = extract_data('/content/dados/ibge.parquet')
 
-df_eixo_i = calcular_eixo_i(df_scr_pix, df_ibge)[cite: 1]
-df_eixo_ii = calcular_eixo_ii(df_scr_pix, df_ibge)[cite: 1]
+df_eixo_i = calcular_eixo_i(df_scr_pix, df_ibge)
+df_eixo_ii = calcular_eixo_ii(df_scr_pix, df_ibge)
 
-df_ibge.drop(columns=["bonusDemografico"], inplace=True)[cite: 1]
+df_ibge.drop(columns=["bonusDemografico"], inplace=True)
 
 # --- Limpeza de segurança contra NaNs e infinitos antes do envio ---
 df_eixo_i = df_eixo_i.replace([np.inf, -np.inf], np.nan).fillna(0)
@@ -19,7 +19,7 @@ df_scr_pix = df_scr_pix.replace([np.inf, -np.inf], np.nan).fillna(0)
 df_ibge = df_ibge.replace([np.inf, -np.inf], np.nan).fillna(0)
 
 # --- Envio das rotas ---                                
-enviar("/data/credit-risk", df_eixo_i.to_dict(orient='records'))[cite: 1]
-enviar("/data/inclusion-expansion", df_eixo_ii.to_dict(orient='records'))[cite: 1]
-enviar("/data/pix-structure", df_scr_pix.to_dict(orient='records'))[cite: 1]
-enviar("/data/ibge-structure", df_ibge.to_dict(orient='records'))[cite:1]
+enviar("/data/credit-risk", df_eixo_i.to_dict(orient='records'))
+enviar("/data/inclusion-expansion", df_eixo_ii.to_dict(orient='records'))
+enviar("/data/pix-structure", df_scr_pix.to_dict(orient='records'))
+enviar("/data/ibge-structure", df_ibge.to_dict(orient='records'))
