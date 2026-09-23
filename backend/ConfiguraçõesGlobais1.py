528d27d32ad650aef06a16d79f216e17a84d8eb2
joao-bordinhon1405 <joaogabrielbordinhon@gmail.com>
2026-09-23 10:09:07 -0300

    adicionando novas colunas a partir de 15 dias

--- backend/ConfiguraçõesGlobais1.py
+++ backend/ConfiguraçõesGlobais1.py
@@ -15,7 +15,7 @@ MAX_GET_RETRIES = 5
 
 #adicionado coluna modalidade
 USECOLS_SCR = ["data_base", "uf", "porte", "cliente", "modalidade", 
-    "carteira_inadimplencia", "carteira_vencida", "carteira_ativa", "vencido_acima_de_90_dias"]
+    "carteira_inadimplencia", "carteira_vencida", "carteira_ativa", "vencido_15_a_60_dias", "vencido_61_a_90_dias", "vencido_acima_de_90_dias"]
 
 ano_anterior = datetime.now().year - 1
 URLS_SCR = [f"https://www.bcb.gov.br/pda/desig/scrdata_{ano}.zip" for ano in range(2021, ano_anterior+1)]
